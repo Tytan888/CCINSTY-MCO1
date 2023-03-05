f578842a00633895301fa8aa1519de05b6461c32
Tyler Tan <tylerjhtan888@gmail.com>
2023-03-06 03:07:32 +0800

    1.5
    Added internal documentation

--- Controller.java
+++ Controller.java
@@ -6,28 +6,37 @@ import java.awt.event.ActionEvent;
 
 public class Controller {
 
+    // Constant for how many miliseconds between each exploration in the A* search.
     private final int DELAY = 50;
 
+    // All the private variables needed for the program to run.
     private View view;
     private Model model;
     private int n;
 
     private Timer mazeTimer;
     private Timer successTimer;
-    private int itest = 1;
 
     private Tile tempGoal;
 
+    /**
+     * This creates a Controller object, which is responsible for creating and
+     * integrating the Model and View objects to be used in the program.
+     */
     public Controller() throws FileNotFoundException {
+        // Initialize the Model and retrieve the size from the inputted maze.txt file.
         this.model = new Model();
         this.n = this.model.getSize();
 
+        // Initialize the View as well as the button's starting actionListener.
         this.view = new View(this.n, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 startSearch();
             }
         });
+
+        // Update all displayed tiles in the GUI.
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 this.view.updateTile(i, j, model.getTile()[i][j].getType(), model.getTile()[i][j].getStatus());
@@ -36,70 +45,102 @@ public class Controller {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+        // Start program by instantiating a new Controller.
         new Controller();
     }
 
+    /**
+     * This method faciliates the starting and the execution of the A* search
+     * process, which includes sending instructions to the Model periodically to
+     * perform the A* process, as well as updating the GUI within View to reflect
+     * changes within said Model.
+     */
     private void startSearch() {
+        // When search is started, change button text...
         this.view.updateButton("Searching...", new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
             }
         }, false);
 
+        // Create new timer to facilitate the A* looping search process.
         this.tempGoal = model.Astar();
         mazeTimer = new Timer(DELAY, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                // If goal is not yet found and frontier is not yet empty...
                 if (tempGoal == null && !model.getFrontier().isEmpty()) {
+                    // Continue performing A*.
                     tempGoal = model.Astar();
+                    // Update all displayed tiles in the GUI.
                     for (int i = 0; i < n; i++) {
                         for (int j = 0; j < n; j++) {
                             view.updateTile(i, j, model.getTile()[i][j].getType(), model.getTile()[i][j].getStatus());
                         }
                     }
-                } else {
+                }
+                // If goal is found or frontier is empty...
+                else {
                     String endMsg = "Exit";
+                    // If goal was not found, meaning that the frontier ended up empty...
                     if (tempGoal == null) {
+                        // Change all explored tiles to red.
                         view.failTiles();
+                        // Change button text to display explored tiles and the failed state.
                         endMsg = "(" + model.getCount() + ") Failed...";
-                    } else {
+                    }
+                    // If goal was found, meaning A* had found an optimal path...
+                    else {
+                        // Set a new timer for tracking the final path.
                         successTimer = new Timer(DELAY, new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
+                                // Backtrack tiles until the start tile is reached.
                                 if (tempGoal.getParent() != null) {
+                                    // Set tile to green (unless they are the goal tile).
                                     if (!(tempGoal.getCoordinate()[0] == model.getGoals()[0]
                                             && tempGoal.getCoordinate()[1] == model.getGoals()[1])) {
-
                                         view.succeedTile(tempGoal.getCoordinate()[0], tempGoal.getCoordinate()[1]);
                                     }
                                     tempGoal = tempGoal.getParent();
-
                                 } else {
+                                    // Once start tile is reached, stop this "success" timer.
                                     successTimer.stop();
                                 }
 
                             }
                         });
+
+                        // Start said "success" timer.
                         successTimer.setRepeats(true);
                         successTimer.start();
                         endMsg = "(" + model.getCount() + ") Success!";
                     }
+
+                    // Change button text and actionListener to reflect the search finishing.
                     view.updateButton(endMsg, new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-
                             endSearch();
-                            System.out.println(itest);
                         }
                     }, true);
+
+                    // Finally, stop this "maze" timer once and for all.
                     mazeTimer.stop();
                 }
             }
         });
+
+        // Set "maze" timer to repeating and start.
         mazeTimer.setRepeats(true);
         mazeTimer.start();
     }
 
+    /**
+     * This function is in charge of terminating the JFrame within View when the
+     * program is instructed to Fend.
+     */
     private void endSearch() {
         this.view.closeFrame();
     }
--- View.java
+++ View.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 
 public class View {
 
+    // All the private variables needed for the GUI to function.
     private JFrame mainFrame;
     private int n;
 
@@ -22,10 +23,16 @@ public class View {
 
     private JButton theButton;
 
+    /**
+     * This creates a View object, which handles all the aspects relating to the GUI
+     * and its functionalities.
+     */
     public View(int n, ActionListener startListener) {
 
+        // Store the size of the maze.
         this.n = n;
 
+        // Instantiate the main JFrame to be used.
         this.mainFrame = new JFrame("Mazefinder 2000");
         this.mainFrame.setSize(750, 800);
         this.mainFrame.setUndecorated(false);
@@ -33,11 +40,13 @@ public class View {
         this.mainFrame.setLocationRelativeTo(null);
         this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Instantiate the JPanel to house the bottom button.
         this.buttonPanel = new JPanel();
         this.buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 5));
         this.buttonPanel.setBackground(Color.black);
         this.buttonPanel.setPreferredSize(new Dimension(1000, 80));
 
+        // Instantiate the only JButton present in the program.
         this.theButton = new JButton("Start");
         this.theButton.addActionListener(startListener);
         this.theButton.setPreferredSize(new Dimension(500, 70));
@@ -48,13 +57,14 @@ public class View {
         this.theButton.setBackground(Color.BLACK);
         this.theButton.setFont(new Font("Helvetica", Font.PLAIN, 40));
         this.theButton.setBorder(BorderFactory.createLineBorder(Color.white, 5));
-
         this.buttonPanel.add(theButton);
 
+        // Instantiate the JPanel to house the main maze.
         this.mazePanel = new JPanel();
         this.mazePanel.setLayout(new GridLayout(n, n));
         this.mazePanel.setBackground(Color.BLACK);
 
+        // Instantiate the many sub-JPanels representing each tile in the maze.
         this.tilePanels = new JPanel[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -68,31 +78,50 @@ public class View {
             }
         }
 
+        // Add buttonPanel and mazePanel to the overall JFrame.
         this.mainFrame.add(this.buttonPanel, BorderLayout.SOUTH);
         this.mainFrame.add(this.mazePanel, BorderLayout.CENTER);
 
+        // Finally set the JFrame to be visible.
         this.mainFrame.setVisible(true);
     }
 
+    /**
+     * This method is tasked with updating the color of a tile, given its position
+     * and state in the maze.
+     * 
+     * @param row          the row coordinate of the tile to be updated
+     * @param col          the column coordinate of the tile to be updated
+     * @param TILE_STATE   the type of the tile (wall, goal, start, free)
+     * @param SEARCH_STATE the search status of the tile (unexplored, frontier,
+     *                     explored)
+     */
     public void updateTile(int row, int col, int TILE_STATE, int SEARCH_STATE) {
         switch (TILE_STATE) {
+            // If tile is a wall, then set the color to black.
             case Tile.WALL:
                 this.tilePanels[row][col].setBackground(Color.black);
                 break;
+            // If tile is the goal, then set the color to orange.
             case Tile.GOAL:
                 this.tilePanels[row][col].setBackground(Color.ORANGE);
                 break;
+            // If tile is the start, then set the color to blue.
             case Tile.START:
                 this.tilePanels[row][col].setBackground(Color.BLUE);
                 break;
+            // If tile is a normal tile...
             case Tile.FREE:
                 switch (SEARCH_STATE) {
+                    // If tile is still unexplored, then set the color to white.
                     case Tile.UNEXPLORED:
                         this.tilePanels[row][col].setBackground(Color.WHITE);
                         break;
+                    // If tile is in the frontier, then set the color to yellow.
                     case Tile.FRONTIER:
                         this.tilePanels[row][col].setBackground(Color.YELLOW);
                         break;
+                    // If tile has been explored, then set the color to purple.
                     case Tile.EXPLORED:
                         this.tilePanels[row][col].setBackground(new Color(156, 81, 182));
                         break;
@@ -101,10 +130,21 @@ public class View {
         }
     }
 
+    /**
+     * This method is tasked with updating the color of a tile to green to indicate
+     * its inclusion in the optimal path, given its position in the maze.
+     * 
+     * @param row the row coordinate of the tile to be updated
+     * @param col the column coordinate of the tile to be updated
+     */
     public void succeedTile(int row, int col) {
         this.tilePanels[row][col].setBackground(Color.green);
     }
 
+    /**
+     * This method is tasked with updating the color of all currently purple tiles
+     * to red to indicate failure of finding a single possible path.
+     */
     public void failTiles() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -115,6 +155,16 @@ public class View {
         }
     }
 
+    /**
+     * This method is created to update the text, actionListener, and enabledStatus
+     * of the bottom JButton.
+     * 
+     * @param text        the String to which the button's text will be set
+     * @param endListener the new actionListenere to which the button's
+     *                    actionListener will be set
+     * @param enable      a boolean value indicating whether the button should be
+     *                    enabled or not
+     */
     public void updateButton(String text, ActionListener endListener, boolean enable) {
         this.theButton.removeActionListener(this.theButton.getActionListeners()[0]);
         this.theButton.setText(text);
@@ -122,6 +172,10 @@ public class View {
         this.theButton.setEnabled(enable);
     }
 
+    /**
+     * This method is solely responsible for closing the JFrame and the overall
+     * program when the user eventually hits the close button.
+     */
     public void closeFrame() {
         this.mainFrame.setVisible(false);
         this.mainFrame.dispose();
